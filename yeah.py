9e710055d28dad641777abe163412cec5723ee00
Lucci02 <146361028+Lucci02@users.noreply.github.com>
2023-11-26 19:33:03 -0600

    Update yeah.py

--- yeah.py
+++ yeah.py
@@ -1,25 +1,28 @@
-cooking_time = 0
-weight = 0
-
 def calculate_cooking_time(weight):
-    # Assuming 15 minutes per pound for cooking time
+    # Assuming 15 minutes per pound for cooking time (you can adjust this as needed)
     cooking_time = weight * 15
+    return cooking_time
 
-
-print("Welcome to the Thanksgiving Turkey Cooking Time Calculator!")
-# Get the weight of the turkey from the user
-weight = str(input("Enter the weight of the turkey in pounds: "))
-zero = float(weight)
-weight0 = float(weight) * 15
-    # Validate that the weight is a positive number
-if zero <= 0:
-    print("Invalid input. Please enter a positive weight.")
-       
+def main():
+    print("Welcome to the Thanksgiving Turkey Cooking Time Calculator!")
+    
+    try:
+        # Get the weight of the turkey from the user
+        weight = float(input("Enter the weight of the turkey in pounds: "))
         
-# Calculate the cooking time
-cooking_time = calculate_cooking_time(weight)
+        # Validate that the weight is a positive number
+        if weight <= 0:
+            print("Invalid input. Please enter a positive weight.")
+            return
         
-# Display the cooking time
-print("Estimated cooking time for a " + str(weight) + " pound turkey: " + str(weight0) + "minutes")
+        # Calculate the cooking time
+        cooking_time = calculate_cooking_time(weight)
+        
+        # Display the cooking time
+        print(f"\nEstimated cooking time for a {weight} pound turkey: {cooking_time} minutes")
+    
+    except ValueError:
+        print("Invalid input. Please enter a valid number for the weight.")
 
-
+if __name__ == "__main__":
+    main()
