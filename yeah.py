c141aa242a9b427d8a00ebc2b16041b95780eb69
Lucci.02 <lm050707@wgcloud.org>
2023-11-28 10:18:44 -0600

    test

--- yeah.py
+++ yeah.py
@@ -10,17 +10,17 @@ print("Welcome to the Thanksgiving Turkey Cooking Time Calculator!")
 # Get the weight of the turkey from the user
 weight = str(input("Enter the weight of the turkey in pounds: "))
 zero = float(weight)
-weight0 = float(weight) * 15
+time = float(weight) * 15
     # Validate that the weight is a positive number
-while zero <= 0:
+while weight <= 0:
     print("Invalid input. Please enter a numerical weight above 0.")
-    weight = str(input("Enter the weight of the turkey in pounds: "))
+    weight = float(input("Enter the weight of the turkey in pounds: "))
        
         
 # Calculate the cooking time
 cooking_time = calculate_cooking_time(weight)
         
 # Display the cooking time
-print("Estimated cooking time for a " + str(weight) + " pound turkey: " + str(weight0) + "minutes")
+print("Estimated cooking time for a " + str(weight) + " pound turkey: " + str(time) + "minutes")
 
 
