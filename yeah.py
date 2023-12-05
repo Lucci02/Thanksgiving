268ea0df46646a3a505103448b83ba55056327f5
Lucci02 <146361028+Lucci02@users.noreply.github.com>
2023-12-04 19:31:38 -0600

    Update yeah.py

--- yeah.py
+++ yeah.py
@@ -1,25 +1,17 @@
-cooking_time = 0
 weight = 0
-
-def calculate_cooking_time(weight):
-    # Assuming 15 minutes per pound for cooking time
-    cooking_time = weight * 15
-
-
+cooking_time = weight * 15
+#intro
 print("Welcome to the Thanksgiving Turkey Cooking Time Calculator!")
-# Get the weight of the turkey from the user
-weight = str(input("Enter the weight of the turkey in pounds: "))
-zero = float(weight)
-weight0 = float(weight) * 15
-    # Validate that the weight is a positive number
-if zero <= 0:
-    print("Invalid input. Please enter a positive weight.")
-       
-        
-# Calculate the cooking time
-cooking_time = calculate_cooking_time(weight)
-        
-# Display the cooking time
-print("Estimated cooking time for a " + str(weight) + " pound turkey: " + str(weight0) + "minutes")
-
-
+#determine input validity
+while True:
+    weight_input = input("Enter the weight of the turkey in pounds: ")
+#Looked up how to do this (if you remember will you explain it to me in class?)
+    if weight_input.replace(".", "", 1).isdigit():
+        weight = float(weight_input)
+        break
+    else:
+        print("Invalid input. Please enter a numeric value for the weight.")
+#calculate
+cooking_time = weight * 15
+#print out value
+print("For a " + str(weight) + "-pound turkey, the estimated cooking time is " + str(cooking_time) + " minutes.")
